1609ffa59257c4eb3000536f4870040b54fe4c54
Kalimaha <guido.barbaglia@gmail.com>
2014-11-28 15:09:05 +0100

    Create tabs for the progress bars.

--- geobricks_ui_download_progress.js
+++ geobricks_ui_download_progress.js
@@ -8,11 +8,14 @@ define(['jquery',
 
     'use strict';
 
-    function UI_MODIS() {
+    function UI_DLWD_PROGRESSS() {
 
         this.CONFIG = {
             lang: 'en',
-            placeholder_id: 'placeholder'
+            placeholder_id: 'placeholder',
+            files_to_be_downloaded: null,
+            tab_label: null,
+            tab_id: null
         };
 
     }
@@ -22,14 +25,56 @@ define(['jquery',
      *
      * @param config Custom configuration in JSON format to extend the default settings.
      */
-    UI_MODIS.prototype.init = function(config) {
+    UI_DLWD_PROGRESSS.prototype.init = function(config) {
 
         /* Extend default configuration. */
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
 
         /* Fix the language, if needed. */
         this.CONFIG.lang = this.CONFIG.lang != null ? this.CONFIG.lang : 'en';
+
+        /* Store selectors. */
+        this.tab_headers = $('#tab_headers');
+        this.tab_contents = $('#tab_contents');
+
+        /* Create progress bar. */
+        console.debug(this.CONFIG.files_to_be_downloaded);
+        this.create_progress_bar_tab();
         
-    }
+    };
+
+    UI_DLWD_PROGRESSS.prototype.create_progress_bar_tab = function() {
+
+        /* Render the tab header. */
+        var template = $(templates).filter('#tab_header').html();
+        var view = {
+            href: this.CONFIG.tab_id,
+            tab_label: this.CONFIG.tab_label
+        };
+        var render = Mustache.render(template, view);
+        this.tab_headers.append(render);
+
+        /* Render the tab content. */
+        template = $(templates).filter('#progress_tab_content').html();
+        view = {
+            id: this.CONFIG.tab_id
+        };
+        render = Mustache.render(template, view);
+        this.tab_contents.append(render);
+
+        /* Create a progress bar for each file to be downloaded. */
+        for (var i = 0 ; i < this.CONFIG.files_to_be_downloaded.length ; i++) {
+            template = $(templates).filter('#progress').html();
+            view = {};
+            render = Mustache.render(template, view);
+            $('#' + this.CONFIG.tab_id).append(render);
+        }
+
+        /* Show the first tab. */
+        this.tab_headers.find('a:nth-child(1)').tab('show');
+
+    };
+
+    return new UI_DLWD_PROGRESSS();
     
 });
